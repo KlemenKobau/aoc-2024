f4e951d0ced9dd7c42b49ddf7d94e46a341b7810
kkobau <klemen.kobau@gmail.com>
2024-12-26 11:56:27 +0100

    finished day 17 part 1

--- src/seventeen.rs
+++ src/seventeen.rs
@@ -1,3 +1,5 @@
+use std::ops::Div;
+
 use crate::util::read_lines;
 
 pub fn main() {
@@ -7,7 +9,7 @@ pub fn main() {
     let c: i64 = read_lines[2].split_once(": ").unwrap().1.parse().unwrap();
     let instructions = read_lines[4].split_once(": ").unwrap().1;
 
-    let machine = Machine::new(Registers { a, b, c }, instructions);
+    let mut machine = Machine::new(Registers { a, b, c }, instructions);
 
     loop {
         let step_success = machine.step();
@@ -15,12 +17,22 @@ pub fn main() {
             break;
         }
     }
+
+    let output = machine
+        .output
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>()
+        .join(",");
+
+    println!("{}", output);
 }
 
 struct Machine {
     registers: Registers,
     instructions: Vec<i64>,
     instruction_pointer: usize,
+    output: Vec<i64>,
 }
 
 struct Registers {
@@ -40,10 +52,11 @@ impl Machine {
             registers,
             instructions,
             instruction_pointer: 0,
+            output: Vec::new(),
         }
     }
 
-    fn step(&self) -> bool {
+    fn step(&mut self) -> bool {
         let instruction_pointer = self.instruction_pointer;
         let instruction = self.instructions.get(instruction_pointer);
         let operand = self.instructions.get(instruction_pointer + 1);
@@ -58,8 +71,26 @@ impl Machine {
         }
 
         let instruction = parse_instruction(instruction.unwrap().clone(), operand.unwrap().clone());
+        self.apply_instruction(instruction);
 
         return true;
+    }
+
+    fn apply_instruction(&mut self, instruction: Instruction) {
+        match instruction {
+            Instruction::Adv { operand } => adv(operand, self),
+            Instruction::Bxl { operand } => bxl(operand, self),
+            Instruction::Bst { operand } => bst(operand, self),
+            Instruction::Jnz { operand } => jnz(operand, self),
+            Instruction::Bxc => bxc(self),
+            Instruction::Out { operand } => out(operand, self),
+            Instruction::Bdv { operand } => bdv(operand, self),
+            Instruction::Cdv { operand } => cdv(operand, self),
+        }
+    }
+
+    fn increase_instruction_pointer(&mut self) {
+        self.instruction_pointer = self.instruction_pointer + 2;
     }
 }
 
@@ -86,4 +117,91 @@ enum Instruction {
     Out { operand: i64 },
     Bdv { operand: i64 },
     Cdv { operand: i64 },
+}
+
+fn bxl(operand: i64, machine: &mut Machine) {
+    let number = machine.registers.b;
+    let res = number ^ operand;
+    machine.registers.b = res;
+
+    machine.increase_instruction_pointer();
+}
+
+fn bst(operand: i64, machine: &mut Machine) {
+    let operand = parse_combo_operator(operand, machine);
+    let res = operand % 8;
+    machine.registers.b = res;
+
+    machine.increase_instruction_pointer();
+}
+
+fn jnz(operand: i64, machine: &mut Machine) {
+    let a = machine.registers.a;
+    if a == 0 {
+        machine.increase_instruction_pointer();
+        return;
+    }
+
+    machine.instruction_pointer = operand.try_into().unwrap();
+}
+
+fn bxc(machine: &mut Machine) {
+    let b = machine.registers.b;
+    let c = machine.registers.c;
+
+    let res = b ^ c;
+    machine.registers.b = res;
+
+    machine.increase_instruction_pointer();
+}
+
+fn out(operand: i64, machine: &mut Machine) {
+    let operand = parse_combo_operator(operand, machine);
+    let mo = operand % 8;
+    machine.output.push(mo);
+
+    machine.increase_instruction_pointer();
+}
+
+fn adv(operand: i64, machine: &mut Machine) {
+    let numerator = machine.registers.a;
+    let operand = parse_combo_operator(operand, machine);
+    let denominator: i64 = 2_i64.pow(operand.try_into().unwrap());
+
+    let res = numerator.div(denominator);
+    machine.registers.a = res;
+
+    machine.increase_instruction_pointer();
+}
+
+fn bdv(operand: i64, machine: &mut Machine) {
+    let numerator = machine.registers.a;
+    let operand = parse_combo_operator(operand, machine);
+    let denominator: i64 = 2_i64.pow(operand.try_into().unwrap());
+
+    let res = numerator.div(denominator);
+    machine.registers.b = res;
+
+    machine.increase_instruction_pointer();
+}
+
+fn cdv(operand: i64, machine: &mut Machine) {
+    let numerator = machine.registers.a;
+    let operand = parse_combo_operator(operand, machine);
+    let denominator: i64 = 2_i64.pow(operand.try_into().unwrap());
+
+    let res = numerator.div(denominator);
+    machine.registers.c = res;
+
+    machine.increase_instruction_pointer();
+}
+
+fn parse_combo_operator(operand: i64, machine: &Machine) -> i64 {
+    match operand {
+        0..=3 => operand,
+        4 => machine.registers.a,
+        5 => machine.registers.b,
+        6 => machine.registers.c,
+        _ => unreachable!(),
+    }
 }
